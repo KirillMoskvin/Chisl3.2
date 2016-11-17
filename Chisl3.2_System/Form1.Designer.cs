705413215bed59dff9e9ee6451ebdf7061434ed0
KirillMoskvin <kirill.moskvin@mail.ru>
2016-11-18 00:20:24 +0300

    2-я задача по численным методам

--- Chisl3.2_System/Form1.Designer.cs
+++ Chisl3.2_System/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Chisl3._2_System
 {
-    partial class Form1
+    partial class Form_main
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,12 +28,153 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.graph = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btn_solve = new System.Windows.Forms.Button();
+            this.nud_eps = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_solution = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.graph)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_eps)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // graph
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.graph.ChartAreas.Add(chartArea2);
+            this.graph.Location = new System.Drawing.Point(12, 73);
+            this.graph.Name = "graph";
+            series2.ChartArea = "ChartArea1";
+            series2.Name = "Series1";
+            this.graph.Series.Add(series2);
+            this.graph.Size = new System.Drawing.Size(501, 317);
+            this.graph.TabIndex = 0;
+            this.graph.Text = "graph";
+            // 
+            // btn_solve
+            // 
+            this.btn_solve.Location = new System.Drawing.Point(157, 37);
+            this.btn_solve.Name = "btn_solve";
+            this.btn_solve.Size = new System.Drawing.Size(72, 20);
+            this.btn_solve.TabIndex = 1;
+            this.btn_solve.Text = "Решить";
+            this.btn_solve.UseVisualStyleBackColor = true;
+            this.btn_solve.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // nud_eps
+            // 
+            this.nud_eps.DecimalPlaces = 4;
+            this.nud_eps.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            262144});
+            this.nud_eps.Location = new System.Drawing.Point(12, 39);
+            this.nud_eps.Name = "nud_eps";
+            this.nud_eps.Size = new System.Drawing.Size(120, 20);
+            this.nud_eps.TabIndex = 3;
+            this.nud_eps.ValueChanged += new System.EventHandler(this.nud_eps_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Погрешность:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tb_solution
+            // 
+            this.tb_solution.Location = new System.Drawing.Point(12, 418);
+            this.tb_solution.Multiline = true;
+            this.tb_solution.Name = "tb_solution";
+            this.tb_solution.Size = new System.Drawing.Size(501, 55);
+            this.tb_solution.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 402);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Решение: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(359, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Система:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(415, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "sin(x-0,6)-y=1,6";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(415, 44);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "3x-cos y = 0,9";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Form_main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(550, 498);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb_solution);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nud_eps);
+            this.Controls.Add(this.btn_solve);
+            this.Controls.Add(this.graph);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form_main";
+            this.Text = "Решение системы нелинейных уравнений методом итераций";
+            this.Load += new System.EventHandler(this.Form_main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.graph)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_eps)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart graph;
+        public System.Windows.Forms.Button btn_solve;
+        private System.Windows.Forms.NumericUpDown nud_eps;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_solution;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- Chisl3.2_System/Form1.cs
+++ Chisl3.2_System/Form1.cs
@@ -7,14 +7,205 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Chisl3._2_System
 {
-    public partial class Form1 : Form
+    public partial class Form_main : Form
     {
-        public Form1()
+        bool ploted = false;
+
+        public Form_main()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// отрисовка гафика и решение
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tb_solution.Clear();
+            if ((double)nud_eps.Value <= 0.0)
+            {
+                MessageBox.Show("Введите положительное eps", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (!ploted)
+                Plot();
+            tb_solution.Text = Solve((double)nud_eps.Value);
+        }
+
+        /// <summary>
+        /// Первая функция
+        /// </summary>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        double phi1(double y)
+        {
+            return (Math.Cos(y) / 3 + 0.3);
+        }
+
+        /// <summary>
+        /// Вторая функция
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        double phi2(double x)
+        {
+            return (Math.Sin(x - 0.6) - 1.6);
+        }
+
+        /// <summary>
+        /// Первая функция, продифференцированная по X (для наглядности вычислений)
+        /// </summary>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        double DiffXPhi1(double y)
+        {
+            return 0;
+        }
+        /// <summary>
+        /// Первая функция, продифференцированная по Y 
+        /// </summary>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        double DiffYPhi1(double y)
+        {
+            return -Math.Sin(y) / 3;
+        }
+
+        /// <summary>
+        /// Первая функция, продифференцированная по Y 
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        double DiffXPhi2(double x)
+        {
+            return Math.Cos(x-0.6);
+        }
+        /// <summary>
+        /// Первая функция, продифференцированная по Y (для наглядности вычислений)
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        double DiffYPhi2(double x)
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Построение графика
+        /// </summary>
+        void Plot()
+        {
+            double start = -10;
+            double finish = 10;
+            double step = 0.5;
+
+            graph.ChartAreas[0].AxisX.ArrowStyle = AxisArrowStyle.Triangle;
+            graph.ChartAreas[0].AxisX.Crossing = 0.0;
+            graph.ChartAreas[0].AxisY.Crossing = 0.0;
+            graph.ChartAreas[0].AxisX.IsMarksNextToAxis = false;
+            graph.ChartAreas[0].AxisY.IsMarksNextToAxis = false;
+
+            graph.ChartAreas[0].AxisX.LineWidth = 2;
+            graph.ChartAreas[0].AxisY.LineWidth = 2;
+            graph.ChartAreas[0].AxisX.Interval = 0.5;
+            graph.ChartAreas[0].AxisY.Interval = 0.5;
+            graph.ChartAreas[0].AxisX.Maximum = 3;
+            graph.ChartAreas[0].AxisX.Minimum = -3;
+            graph.ChartAreas[0].AxisY.Maximum = 3;
+            graph.ChartAreas[0].AxisY.Minimum = -3;
+            graph.Series[0].ChartType = SeriesChartType.Spline;
+
+            for (double x = start; x < finish; x += step)
+                graph.Series[0].Points.AddXY(x, phi2(x));
+
+            graph.Series.Add("Graph2");
+            graph.Series[1].ChartType = SeriesChartType.Spline;
+            graph.Series[1].Color = Color.Red;
+
+            for (double y = start; y < finish; y += step)
+                graph.Series[1].Points.AddXY(phi1(y), y);
+            ploted = true;
+        }
+        /// <summary>
+        /// Оценка погрешности
+        /// </summary>
+        /// <param name="xCurr"></param>
+        /// <param name="xPrev"></param>
+        /// <param name="yCurr"></param>
+        /// <param name="yPrev"></param>
+        /// <returns></returns>
+        double EvaluateError(double xCurr, double xPrev, double yCurr, double yPrev)
+        {
+            double q1 = Math.Abs(DiffXPhi1(yCurr)) + Math.Abs(DiffYPhi1(yCurr));
+            double q2 = Math.Abs(DiffXPhi2(xCurr)) + Math.Abs(DiffYPhi2(xCurr));
+            double M = Math.Min(q1, q2);
+
+            return (M / (1 - M)) * (Math.Abs(xCurr - xPrev) + Math.Abs(yCurr - yPrev));
+        }
+
+        /// <summary>
+        /// Рассчет корней
+        /// </summary>
+        /// <param name="eps"></param>
+        /// <returns></returns>
+        string Solve(double eps)
+        {
+            double xCurr = 0.15;
+            double yCurr = -2;
+            double xPrev = 0, yPrev = 0;
+            double error = 0;
+            int step = 0;
+
+            do
+            {
+                ++step;
+                xPrev = xCurr;
+                yPrev = yCurr;
+                xCurr = phi1(yCurr);
+                yCurr = phi2(xCurr);
+                error = EvaluateError(xCurr, xPrev, yCurr, yPrev);
+            } while (Math.Abs(error) > eps);
+
+            return ("Шагов проделано: " + step.ToString() + "\r\n" +
+                "Полученное значение x: " + xCurr.ToString() + "\r\n" +
+                "Полученное значение y: " + yCurr.ToString() + "\r\n");
+            
+        }
+        
+        private void Form_main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nud_eps_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
